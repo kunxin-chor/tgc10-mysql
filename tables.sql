71ad69357aab7dbc915ec760ceb2e3547737aee4
kunxin-chor <chorkunxin@yahoo.com>
2021-02-03 13:25:11 +0000

    Push sample queries

--- tables.sql
+++ tables.sql
@@ -1,5 +1,20 @@
+# to go into MySQL
+mysql -u root
+
+# see all the existing databases
+show databases;
+
+# create a new database
+create database hrm;
+
+# switch the current active database
+use hrm;
+
+# see all the tables in the database
+show tables;
+
 create table Departments (
   department_id int unsigned auto_increment primary key,
   department_name varchar(200) not null,
-  email varchar(200 not null
+  email varchar(200) not null
 ) engine=innodb;
