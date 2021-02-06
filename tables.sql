117ca5f9b48948a21b680d20b54d9bf1a925aec6
kunxin-chor <chorkunxin@yahoo.com>
2021-02-06 06:16:11 +0000

    Added queries to showcase AND vs. OR

--- tables.sql
+++ tables.sql
@@ -86,3 +86,42 @@ create table Pets (
 this means, the row with pet_onwer_id owns Muffin */
 insert into Pets (name, pet_owner_id) values ('Muffin', 1);
 insert into Pets (name, pet_owner_id) values ('Biscuit', 1);
+
+/* CANNOT insert a pet for an non-existent pet owner */
+insert into Pets (name, pet_owner_id) values ('Smaug', 100);
+/* CANNOT delete a pet owner while they still have pets */
+delete from PetOwners where pet_owner_id = 1;
+
+/* Select columns from all rows in a table */
+select * from employees;
+
+/* select the lastName, firstName, email and jobTitle */
+select lastName, firstName, email, jobTitle from employees;
+
+/* only keep all the employees from officeCode 1 */
+select * from employees where officeCode = 1;
+
+/* onlly show all empployees that have job title 'Sales Rep' */
+select * from employees where jobTitle = 'Sales Rep'
+
+/* only show the first name, last name and email from all emplyoees that have the job title 'Sales Rep' */
+select firstName, lastName, email from employees where jobTitle = "Sales Rep";
+
+/* show all emplyoees who are sales rep in office code 1 */
+select * from employees where officeCode = 1 and jobTitle='Sales Rep';
+
+/* select all the employees from office code 1 and office code 2 */
+SELECT * FROM employees where officeCode =1 or officeCode=2;
+
+/* select all emplyoees from office code 2 and ONLY sales rep from office code 1 */
+SELECT * FROM employees where officeCode =2 or officeCode=1 and jobTitle="Sales Rep";
+
+/* select only sales rep from office code 1 and office code 2 */
+select * from employees where (officeCode = 1 or officeCode =2 ) and jobTitle = "Sales Rep";
+
+/* find all the customers from the USA with credit limit > 100000, or any customers from France */
+select * from customers where country ="France" or country ="USA" and creditLimit > 100000;
+
+/* find all customers with credit limit > 100000 and who are either France or USA */
+select * from customers where (country = "France" or country="USA") and creditLimit > 100000;
+
