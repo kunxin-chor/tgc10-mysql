178818aa29885d21af7982bfe18b32f8b953e25f
kunxin-chor <chorkunxin@yahoo.com>
2021-02-06 03:28:01 +0000

    Added insert examples

--- tables.sql
+++ tables.sql
@@ -18,3 +18,35 @@ create table Departments (
   department_name varchar(200) not null,
   email varchar(200) not null
 ) engine=innodb;
+
+create table PetOwners (
+  pet_id int unsigned auto_increment primary key,
+  fname varchar(30) not null,
+  lname varchar(30) not null,
+  email varchar(50) not null
+) engine=innodb;
+
+create table Medicines (
+    medicine_id int unsigned auto_increment primary key,
+    name varchar(50) not null,
+    description text not null 
+) engine = innodb;
+
+create table Vets (
+    vet_id int unsigned auto_increment primary key,
+    fname varchar(30) not null,
+    lname varchar(30) not null
+) engine = innodb;
+
+/* add a new column `office_number` to the Vets table */
+alter table Vets add office_number varchar(20) not null;
+
+/* see all the columns inside a table */
+describe Vets;
+
+/* to add new rows to a table */
+insert into Vets (fname, lname, office_number)
+ values ("Xavier", "Tan", "+65121212");
+
+ /* select all the rows from a table */
+ select * from Vets;
